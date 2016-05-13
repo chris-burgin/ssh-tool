3d7a1f729e3895b8712df6a4b1a8c31e5d07dcd1
ChrisBurgin <chrisburgin95@gmail.com>
2016-05-13 13:05:21 -0400

    Adding Machines

--- public/js/index.min.js
+++ public/js/index.min.js
@@ -49,6 +49,27 @@
     $("body").on('click', '.search_wrapper .add_icon, .add_machine_dialog .back_button img', function (event){
       $('.add_machine_dialog').toggleClass('visible');
     });
+
+    // add form submit
+    $("body").on('submit', '.add_machine_dialog form', function (event){
+      event.preventDefault();
+
+      // set data
+      let title = $(this).children('.title').val();
+      let ip = $(this).children('.ip').val();
+      let user = $(this).children('.user').val();
+
+      // add machine
+      data.add_machine(title, ip, user);
+
+      // render machines
+      render.machines(function(machine_html){
+        $('.machines').html('');
+        $('.machines').prepend(machine_html);
+      });
+
+      $('.add_machine_dialog').removeClass('visible');
+    });
   }; init();
 
 }());
